cb3c4dc9d040a323315da68e669594bf4dd59901
MarivoneAraujo <74380088+MarivoneAraujo@users.noreply.github.com>
2021-04-23 13:51:49 -0300

    Day5: whole player (play, shuffle, etc)

--- podcastrnext/src/components/Player/index.tsx
+++ podcastrnext/src/components/Player/index.tsx
@@ -1,32 +1,64 @@
 import Image from "next/image";
-import { useContext, useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
-import { PlayerContext } from "../../contexts/PlayerContext";
+import { usePlayer } from "../../contexts/PlayerContext";
 import styles from "./styles.module.scss";
+import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
 
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const [progress, setProgress] = useState(0);
 
   const {
     episodeList,
     currentEpisodeIndex,
     isPlaying,
+    isLooping,
+    isShuffling,
     toggleplay,
+    toogleloop,
+    toogleshuffle,
     setPlayingState,
-  } = useContext(PlayerContext);
+    playNext,
+    playPrevious,
+    hasNext,
+    hasPrevious,
+    clearPlayerState
+  } = usePlayer();
 
-  useEffect(() =>{
-    if (!audioRef.current){
+  useEffect(() => {
+    if (!audioRef.current) {
       return;
     }
-    if (isPlaying){
-      audioRef.current.play()
-    } else(
-      audioRef.current.pause()
-    )
+    if (isPlaying) {
+      audioRef.current.play();
+    } else audioRef.current.pause();
+  }, [isPlaying]);
 
-  }, [isPlaying])
+  function setupProgressListener() {
+    audioRef.current.currentTime = 0;
+
+    audioRef.current.addEventListener("timeupdate", () => {
+      setProgress(Math.floor(audioRef.current.currentTime));
+    });
+  }
+
+  function handleSeek(amount:number){
+    audioRef.current.currentTime = amount;
+    setProgress(amount);
+  }
+
+  
+  function handleEpisodeEnded(){
+
+    if (hasNext){
+      playNext()
+    }else{
+      clearPlayerState()
+    }
+  }
+
 
   const episode = episodeList[currentEpisodeIndex];
 
@@ -56,10 +88,14 @@ export function Player() {
 
       <footer className={!episode ? styles.empty : ""}>
         <div className={styles.progress}>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(progress)}</span>
+
           <div className={styles.slider}>
             {episode ? (
               <Slider
+                max={episode.duration}
+                value={progress}
+                onChange={handleSeek}
                 trackStyle={{ backgroundColor: "#04d361" }}
                 railStyle={{ backgroundColor: "#9f75ff" }}
                 handleStyle={{ backgroundColor: "#04d361" }}
@@ -68,23 +104,37 @@ export function Player() {
               <div className={styles.emptySlider} />
             )}
           </div>
-          <span>00:00</span>
+          <span>{convertDurationToTimeString(episode?.duration ?? 0)}</span>
         </div>
 
-        {episode && <audio 
-        src={episode.url} 
-        autoPlay 
-        ref={audioRef} 
-        onPlay={() => setPlayingState(true)}
-        onPause={() => setPlayingState(false)}
-        />}
+        {episode && (
+          <audio
+            src={episode.url}
+            autoPlay
+            onEnded={handleEpisodeEnded}
+            loop={isLooping}
+            ref={audioRef}
+            onLoadedMetadata={setupProgressListener}
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
+          />
+        )}
 
         <div className={styles.buttons}>
-          <button type="button" disabled={!episode}>
+          <button
+            type="button"
+            disabled={!episode || episodeList.length === 1}
+            onClick={toogleshuffle}
+            className={isShuffling ? styles.isActive : ""}
+          >
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
 
-          <button type="button" disabled={!episode}>
+          <button
+            type="button"
+            onClick={playPrevious}
+            disabled={!episode || !hasPrevious}
+          >
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
 
@@ -101,11 +151,20 @@ export function Player() {
             )}
           </button>
 
-          <button type="button" disabled={!episode}>
+          <button
+            type="button"
+            onClick={playNext}
+            disabled={!episode || !hasNext}
+          >
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
 
-          <button type="button" disabled={!episode}>
+          <button
+            type="button"
+            disabled={!episode}
+            onClick={toogleloop}
+            className={isLooping ? styles.isActive : ""}
+          >
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
--- podcastrnext/src/pages/_app.tsx
+++ podcastrnext/src/pages/_app.tsx
@@ -2,34 +2,13 @@ import "../styles/global.scss";
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
 import styles from "../styles/app.module.scss";
-import { PlayerContext } from "../contexts/PlayerContext";
-import { useState } from "react";
+import { PlayerContextProvider } from "../contexts/PlayerContext";
+
 
 function MyApp({ Component, pageProps }) {
 
-  const [episodeList, setEpisodeList] = useState([])
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  } 
-
-
-  function toggleplay() {
-    setIsPlaying(!isPlaying);
-  }
-
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
-  return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, toggleplay, setPlayingState }}>
-
+return(
+  <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -37,7 +16,7 @@ function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+      </PlayerContextProvider>
   );
 }
 
